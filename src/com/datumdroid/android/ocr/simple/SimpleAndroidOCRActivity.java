0e93ea44cc25e95e2de91f45a80ec2a49cdfea32
Justin Zhao <justin.zhao71@gmail.com>
2013-11-09 09:50:29 -0500

    Working on POSTing

--- src/com/datumdroid/android/ocr/simple/SimpleAndroidOCRActivity.java
+++ src/com/datumdroid/android/ocr/simple/SimpleAndroidOCRActivity.java
@@ -1,11 +1,17 @@
 package com.datumdroid.android.ocr.simple;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FilterOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.zip.GZIPInputStream;
 
 import android.app.Activity;
@@ -26,11 +32,28 @@ import android.widget.EditText;
 
 import com.googlecode.tesseract.android.TessBaseAPI;
 
+import org.apache.http.Header;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+
+// HTTP POST stuff
+import org.apache.http.entity.mime.HttpMultipartMode;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.entity.mime.content.FileBody;
+import org.apache.http.entity.mime.content.StringBody;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.protocol.BasicHttpContext;
+import org.apache.http.protocol.HttpContext;
+
 public class SimpleAndroidOCRActivity extends Activity {
 	public static final String PACKAGE_NAME = "com.datumdroid.android.ocr.simple";
 	public static final String DATA_PATH = Environment
 			.getExternalStorageDirectory().toString() + "/SimpleAndroidOCR/";
-	
+
 	// You should have the trained data file in assets folder
 	// You can get them at:
 	// http://code.google.com/p/tesseract-ocr/downloads/list
@@ -55,7 +78,8 @@ public class SimpleAndroidOCRActivity extends Activity {
 			File dir = new File(path);
 			if (!dir.exists()) {
 				if (!dir.mkdirs()) {
-					Log.v(TAG, "ERROR: Creation of directory " + path + " on sdcard failed");
+					Log.v(TAG, "ERROR: Creation of directory " + path
+							+ " on sdcard failed");
 					return;
 				} else {
 					Log.v(TAG, "Created directory " + path + " on sdcard");
@@ -63,34 +87,38 @@ public class SimpleAndroidOCRActivity extends Activity {
 			}
 
 		}
-		
+
 		// lang.traineddata file with the app (in assets folder)
 		// You can get them at:
 		// http://code.google.com/p/tesseract-ocr/downloads/list
 		// This area needs work and optimization
-		if (!(new File(DATA_PATH + "tessdata/" + lang + ".traineddata")).exists()) {
+		if (!(new File(DATA_PATH + "tessdata/" + lang + ".traineddata"))
+				.exists()) {
 			try {
 
 				AssetManager assetManager = getAssets();
-				InputStream in = assetManager.open("tessdata/" + lang + ".traineddata");
-				//GZIPInputStream gin = new GZIPInputStream(in);
-				OutputStream out = new FileOutputStream(DATA_PATH
-						+ "tessdata/" + lang + ".traineddata");
+				InputStream in = assetManager.open("tessdata/" + lang
+						+ ".traineddata");
+				// GZIPInputStream gin = new GZIPInputStream(in);
+				OutputStream out = new FileOutputStream(DATA_PATH + "tessdata/"
+						+ lang + ".traineddata");
 
 				// Transfer bytes from in to out
 				byte[] buf = new byte[1024];
 				int len;
-				//while ((lenf = gin.read(buff)) > 0) {
+				// while ((lenf = gin.read(buff)) > 0) {
 				while ((len = in.read(buf)) > 0) {
 					out.write(buf, 0, len);
 				}
 				in.close();
-				//gin.close();
+				// gin.close();
 				out.close();
-				
+
 				Log.v(TAG, "Copied " + lang + " traineddata");
 			} catch (IOException e) {
-				Log.e(TAG, "Was unable to copy " + lang + " traineddata " + e.toString());
+				Log.e(TAG,
+						"Was unable to copy " + lang + " traineddata "
+								+ e.toString());
 			}
 		}
 
@@ -205,38 +233,254 @@ public class SimpleAndroidOCRActivity extends Activity {
 		}
 
 		// _image.setImageBitmap( bitmap );
-		
+
 		Log.v(TAG, "Before baseApi");
 
+		// TessBaseAPI Stuff
 		TessBaseAPI baseApi = new TessBaseAPI();
 		baseApi.setDebug(true);
 		baseApi.init(DATA_PATH, lang);
 		baseApi.setImage(bitmap);
-		
 		String recognizedText = baseApi.getUTF8Text();
-		
 		baseApi.end();
-
-		// You now have the text in recognizedText var, you can do anything with it.
-		// We will display a stripped out trimmed alpha-numeric version of it (if lang is eng)
+		
+		// You now have the text in recognizedText var, you can do anything with
+		// it.
+		// We will display a stripped out trimmed alpha-numeric version of it
+		// (if lang is eng)
 		// so that garbage doesn't make it to the display.
 
 		Log.v(TAG, "OCRED TEXT: " + recognizedText);
 
-		if ( lang.equalsIgnoreCase("eng") ) {
+		if (lang.equalsIgnoreCase("eng")) {
 			recognizedText = recognizedText.replaceAll("[^a-zA-Z0-9]+", " ");
 		}
-		
+
 		recognizedText = recognizedText.trim();
 
-		if ( recognizedText.length() != 0 ) {
-			_field.setText(_field.getText().toString().length() == 0 ? recognizedText : _field.getText() + " " + recognizedText);
+		if (recognizedText.length() != 0) {
+			_field.setText(_field.getText().toString().length() == 0 ? recognizedText
+					: _field.getText() + " " + recognizedText);
 			_field.setSelection(_field.getText().toString().length());
 		}
-		
+
 		// Cycle done.
+
+
+		// Preparing the image for sending
+		ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 90, stream); //compress to which format you want.
+        byte [] byte_arr = stream.toByteArray();
+        String image_str = Base64.encodeBytes(byte_arr);
+        ArrayList<NameValuePair> nameValuePairs = new  ArrayList<NameValuePair>();
+        nameValuePairs.add(new BasicNameValuePair("image",image_str));
+        
+	}
+
+	/*
+	public void post(String url, List nameValuePairs) {
+		HttpClient httpClient = new DefaultHttpClient();
+		HttpContext localContext = new BasicHttpContext();
+		HttpPost httpPost = new HttpPost(url);
+
+		try {
+			MultipartEntityBuilder entityBuilder = MultipartEntityBuilder.create();
+			entityBuilder.setMode(HttpMultipartMode.BROWSER_COMPATIBLE);
+
+			for (int index = 0; index < nameValuePairs.size(); index++) {
+				if (nameValuePairs.get(index).getName()
+						.equalsIgnoreCase("image")) {
+					// If the key equals to "image", we use FileBody to transfer
+					// the data
+					entityBuilder.addPart(nameValuePairs.get(index).getName(),
+							new FileBody(new File(nameValuePairs.get(index)
+									.getValue())));
+				} else {
+					// Normal string data
+					entityBuilder.addPart(
+							nameValuePairs.get(index).getName(),
+							new StringBody(nameValuePairs.get(index).getValue()));
+				}
+			}
+
+			httpPost.setEntity(entity);
+
+			HttpResponse response = httpClient.execute(httpPost, localContext);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	*/
+	
+	// TAKEN FROM http://stackoverflow.com/questions/18964288/upload-a-file-through-an-http-form-via-multipartentitybuilder-with-a-progress
+	public static String postFile(String fileName, String userName, String password, String macAddress) throws Exception {
+
+	    HttpClient client = new DefaultHttpClient();
+	    HttpPost post = new HttpPost("http://10.0.2.2/Upload_image_ANDROID/upload_image.php");
+	    MultipartEntityBuilder builder = MultipartEntityBuilder.create();        
+	    builder.setMode(HttpMultipartMode.BROWSER_COMPATIBLE);
+
+	    final File file = new File(fileName);
+	    FileBody fb = new FileBody(file);
+
+	    builder.addPart("file", fb);  
+	    //builder.addTextBody("userName", userName);
+	    //builder.addTextBody("password", password);
+	    //builder.addTextBody("macAddress",  macAddress);
+	    final HttpEntity yourEntity = builder.build();
+
+	    class ProgressiveEntity implements HttpEntity {
+	        @Override
+	        public void consumeContent() throws IOException {
+	            yourEntity.consumeContent();                
+	        }
+	        @Override
+	        public InputStream getContent() throws IOException,
+	                IllegalStateException {
+	            return yourEntity.getContent();
+	        }
+	        @Override
+	        public Header getContentEncoding() {             
+	            return yourEntity.getContentEncoding();
+	        }
+	        @Override
+	        public long getContentLength() {
+	            return yourEntity.getContentLength();
+	        }
+	        @Override
+	        public Header getContentType() {
+	            return yourEntity.getContentType();
+	        }
+	        @Override
+	        public boolean isChunked() {             
+	            return yourEntity.isChunked();
+	        }
+	        @Override
+	        public boolean isRepeatable() {
+	            return yourEntity.isRepeatable();
+	        }
+	        @Override
+	        public boolean isStreaming() {             
+	            return yourEntity.isStreaming();
+	        } // CONSIDER put a _real_ delegator into here!
+
+	        @Override
+	        public void writeTo(OutputStream outstream) throws IOException {
+
+	            class ProxyOutputStream extends FilterOutputStream {
+	                /**
+	                 * @author Stephen Colebourne
+	                 */
+
+	                public ProxyOutputStream(OutputStream proxy) {
+	                    super(proxy);    
+	                }
+	                public void write(int idx) throws IOException {
+	                    out.write(idx);
+	                }
+	                public void write(byte[] bts) throws IOException {
+	                    out.write(bts);
+	                }
+	                public void write(byte[] bts, int st, int end) throws IOException {
+	                    out.write(bts, st, end);
+	                }
+	                public void flush() throws IOException {
+	                    out.flush();
+	                }
+	                public void close() throws IOException {
+	                    out.close();
+	                }
+	            } // CONSIDER import this class (and risk more Jar File Hell)
+
+	            class ProgressiveOutputStream extends ProxyOutputStream {
+	                public ProgressiveOutputStream(OutputStream proxy) {
+	                    super(proxy);
+	                }
+	                public void write(byte[] bts, int st, int end) throws IOException {
+
+	                    // FIXME  Put your progress bar stuff here!
+
+	                    out.write(bts, st, end);
+	                }
+	            }
+
+	            yourEntity.writeTo(new ProgressiveOutputStream(outstream));
+	        }
+
+	    };
+	    ProgressiveEntity myEntity = new ProgressiveEntity();
+
+	    post.setEntity(myEntity);
+	    HttpResponse response = client.execute(post);        
+
+	    return getContent(response);
+
+	} 
+
+	public static String getContent(HttpResponse response) throws IOException {
+	    BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+	    String body = "";
+	    String content = "";
+
+	    while ((body = rd.readLine()) != null) 
+	    {
+	        content += body + "\n";
+	    }
+	    return content.trim();
 	}
 	
-	// www.Gaut.am was here
-	// Thanks for reading!
+	
+	public String convertResponseToString(HttpResponse response) throws IllegalStateException, IOException{
+		 
+        String res = "";
+        StringBuffer buffer = new StringBuffer();
+        inputStream = response.getEntity().getContent();
+        int contentLength = (int) response.getEntity().getContentLength(); //getting content lengthÉ..
+         runOnUiThread(new Runnable() {
+        
+       @Override
+       public void run() {
+           Toast.makeText(UploadImage.this, "contentLength : " + contentLength, Toast.LENGTH_LONG).show();                     
+       }
+   });
+     
+        if (contentLength < 0){
+        }
+        else{
+               byte[] data = new byte[512];
+               int len = 0;
+               try
+               {
+                   while (-1 != (len = inputStream.read(data)) )
+                   {
+                       buffer.append(new String(data, 0, len)); //converting to string and appending  to stringbufferÉ..
+                   }
+               }
+               catch (IOException e)
+               {
+                   e.printStackTrace();
+               }
+               try
+               {
+                   inputStream.close(); // closing the streamÉ..
+               }
+               catch (IOException e)
+               {
+                   e.printStackTrace();
+               }
+               res = buffer.toString();     // converting stringbuffer to stringÉ..
+
+               runOnUiThread(new Runnable() {
+                
+               @Override
+               public void run() {
+                  Toast.makeText(UploadImage.this, "Result : " + res, Toast.LENGTH_LONG).show();
+               }
+           });
+               //System.out.println("Response => " +  EntityUtils.toString(response.getEntity()));
+        }
+        return res;
+   }
+}
+
 }
